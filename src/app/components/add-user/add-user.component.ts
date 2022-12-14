d4cefde76ad0953162f7dd783b0d4a68712fe287
Moh0901 <mohitverma.540.mv@gmail.com>
2022-12-14 12:24:58 +0530

    second

--- src/app/components/add-user/add-user.component.ts
+++ src/app/components/add-user/add-user.component.ts
@@ -11,44 +11,64 @@ import { LoanAppService } from 'src/app/Services/loan-app.service';
 })
 export class AddUserComponent implements OnInit {
 
-  constructor(private loanAppService: LoanAppService, private router:Router,private toastr: ToastrService) { }
+  arrayUsername: string[] = [];
+  users: []= [];
+  constructor(private loanAppService: LoanAppService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.loanAppService.getAllUsers()
+      .subscribe({
+        next: (user) => {
+          this.users = user;
+          this.arrayUsername = user.map((x: any) => x.username.toLowerCase());
+          console.log(this.arrayUsername);
+        },
+        error: (response) => {
+          console.log(response);
+        }
+      });
   }
 
-  addUserForm= new FormGroup({
-    name: new FormControl("",[Validators.required, Validators.pattern("[a-zA-Z].*") ]),
-    username: new FormControl("",[Validators.required, Validators.pattern("[a-zA-Z].*") ]),
-    password: new FormControl("",[Validators.required, Validators.minLength(5), Validators.maxLength(15)]),
-    role: new FormControl("",[Validators.required]),
+  addUserForm = new FormGroup({
+    name: new FormControl("", [Validators.required, Validators.pattern("[a-zA-Z]*[ ]*[a-zA-Z]*")]),
+    username: new FormControl("", [Validators.required, Validators.pattern("[a-zA-Z].*"),this.chkuserValidator.bind(this)]),
+    password: new FormControl("", [Validators.required, Validators.minLength(5), Validators.maxLength(15)]),
+    role: new FormControl("", [Validators.required]),
   });
 
-  addUserSubmitted(){
+  addUserSubmitted() {
     this.loanAppService.addUser({
       name: this.addUserForm.value.name,
       username: this.addUserForm.value.username,
       password: this.addUserForm.value.password,
       role: this.addUserForm.value.role,
-    }).subscribe(res=>{
+    }).subscribe(res => {
       console.log(res);
       this.toastr.success("User Added Suceessfully")
       this.router.navigate(["/users"]);
     })
   }
 
-get Name(): FormControl{
-  return this.addUserForm.get("name") as FormControl;
-}
+  chkuserValidator(control:FormControl){
+    if(this.arrayUsername.indexOf(control.value.toLowerCase())!==-1){
+      return {'nameIsNotAllowed':true}
+    }
+    return null;
+  }
 
-get userName(): FormControl{
-  return this.addUserForm.get("username") as FormControl;
-}
+  get Name(): FormControl {
+    return this.addUserForm.get("name") as FormControl;
+  }
 
-get Password(): FormControl{
-  return this.addUserForm.get("password") as FormControl;
-}
+  get userName(): FormControl {
+    return this.addUserForm.get("username") as FormControl;
+  }
 
-get Role(): FormControl{
-  return this.addUserForm.get("role") as FormControl;
-}
+  get Password(): FormControl {
+    return this.addUserForm.get("password") as FormControl;
+  }
+
+  get Role(): FormControl {
+    return this.addUserForm.get("role") as FormControl;
+  }
 }
